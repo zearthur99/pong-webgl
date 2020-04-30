3c7ebc3619130b12564ded7be28a8cc2b7189bf2
José Arthur <ze_arthur99@hotmail.com>
2020-04-29 21:04:44 -0300

    refactor: limpeza de codigo e prototipo de paddle

--- Renderer.js
+++ Renderer.js
@@ -6,6 +6,7 @@ export default class Renderer {
     this.gl = this.canvas.getContext("webgl") || this.canvas.getContext("experimental-webgl");
     this.gl.viewportWidth = this.canvas.width;
     this.gl.viewportHeight = this.canvas.height;
+    console.log({width: this.gl.viewportWidth, height: this.gl.viewportHeight});
     this.resolution = [this.canvas.width, this.canvas.height, 1.0];
 
     if (!this.gl) {
@@ -41,49 +42,6 @@ export default class Renderer {
       },
     };
 
-    this.buffers = this.initBuffers();
-    this.buffers2 = this.initBuffers2();
-
-  }
-
-  initBuffers() {
-    const positionBuffer = this.gl.createBuffer();
-
-    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, positionBuffer);
-
-    const positions = [
-      -1.0,  1.0,
-      1.0,  1.0,
-      -1.0, -1.0,
-      1.0, -1.0,
-    ];
-
-    this.gl.bufferData(this.gl.ARRAY_BUFFER,
-      new Float32Array(positions),
-      this.gl.STATIC_DRAW);
-
-    return {
-      position: positionBuffer,
-    };
-  }
-
-  initBuffers2() {
-    const positionBuffer = this.gl.createBuffer();
-
-    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, positionBuffer);
-
-    const positions = [
-      -4.0,  3.0,
-      -3.0,  3.0,
-      -2.0, -2.0,
-      1.0, -1.0,
-    ];
-
-    this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(positions), this.gl.STATIC_DRAW);
-
-    return {
-      position: positionBuffer,
-    };
   }
 
   initShaderProgram(vsSource, fsSource) {
@@ -123,57 +81,32 @@ export default class Renderer {
     if (this.first > 0.9) {
       this.first = 0;
     }
-    this.gl.clearColor(Math.random(), 0.0, 0.0, 1.0);  // Clear to black, fully opaque
-    this.gl.clearDepth(1.0);                 // Clear everything
-    this.gl.enable(this.gl.DEPTH_TEST);           // Enable depth testing
-    this.gl.depthFunc(this.gl.LEQUAL);            // Near things obscure far things
-
-    // Clear the canvas before we start drawing on it.
+    this.gl.clearColor(Math.random(), 0.0, 0.0, 1.0);
+    this.gl.clearDepth(1.0);
+    this.gl.enable(this.gl.DEPTH_TEST);
+    this.gl.depthFunc(this.gl.LEQUAL);
 
     this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
 
-    // Create a perspective matrix, a special matrix that is
-    // used to simulate the distortion of perspective in a camera.
-    // Our field of view is 45 degrees, with a width/height
-    // ratio that matches the display size of the canvas
-    // and we only want to see objects between 0.1 units
-    // and 100 units away from the camera.
-
-    const fieldOfView = 45 * Math.PI / 180;   // in radians
+    const fieldOfView = 45 * Math.PI / 180;
     const aspect = this.gl.canvas.clientWidth / this.gl.canvas.clientHeight;
     const zNear = 0.1;
     const zFar = 100.0;
     const projectionMatrix = glMatrix.mat4.create();
 
-    // note: this.glmatrix.js always has the first argument
-    // as the destination to receive the result.
     glMatrix.mat4.perspective(projectionMatrix,
       fieldOfView,
       aspect,
       zNear,
       zFar);
 
-    // Set the drawing position to the "identity" point, which is
-    // the center of the scene.
     const modelViewMatrix = glMatrix.mat4.create();
 
-    // Now move the drawing position a bit to where we want to
-    // start drawing the square.
-
-    glMatrix.mat4.translate(modelViewMatrix,     // destination matrix
-      modelViewMatrix,     // matrix to translate
-      [-0.0, 0.0, -6.0]);  // amount to translate
-
-    // Tell Webthis.gl how to pull out the positions from the position
-    // buffer into the vertexPosition attribute.
-
-
-    // Tell Webthis.gl to use our program when drawing
+    glMatrix.mat4.translate(modelViewMatrix,
+      modelViewMatrix,
+      [-0.0, 0.0, -10.0]);
 
     this.gl.useProgram(this.programInfo.program);
-
-    // Set the shader uniforms
-
     this.gl.uniformMatrix4fv(
       this.programInfo.uniformLocations.projectionMatrix,
       false,
@@ -183,46 +116,51 @@ export default class Renderer {
       false,
       modelViewMatrix);
 
-    {
-      const offset = 0;
-      const vertexCount = 4;
-      this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffers.position);
-      {
-        const numComponents = 2;  // pull out 2 values per iteration
-        const type = this.gl.FLOAT;    // the data in the buffer is 32bit floats
-        const normalize = false;  // don't normalize
-        const stride = 0;         // how many bytes to get from one set of values to the next
-                                  // 0 = use type and numComponents above
-        const offset = 0;         // how many bytes inside the buffer to start from
-        this.gl.vertexAttribPointer(
-          this.programInfo.attribLocations.vertexPosition,
-          numComponents,
-          type,
-          normalize,
-          stride,
-          offset);
-        this.gl.enableVertexAttribArray(this.programInfo.attribLocations.vertexPosition);
-      }
-      this.gl.drawArrays(this.gl.TRIANGLE_STRIP, offset, vertexCount);
-
-      this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffers2.position);
-      {
-        const numComponents = 2;  // pull out 2 values per iteration
-        const type = this.gl.FLOAT;    // the data in the buffer is 32bit floats
-        const normalize = false;  // don't normalize
-        const stride = 0;         // how many bytes to get from one set of values to the next
-                                  // 0 = use type and numComponents above
-        const offset = 0;         // how many bytes inside the buffer to start from
-        this.gl.vertexAttribPointer(
-          this.programInfo.attribLocations.vertexPosition,
-          numComponents,
-          type,
-          normalize,
-          stride,
-          offset);
-        this.gl.enableVertexAttribArray(this.programInfo.attribLocations.vertexPosition);
-      }
-      this.gl.drawArrays(this.gl.TRIANGLE_STRIP, offset, vertexCount);
-    }
+    this.drawPaddles();
+    this.drawBall();
   }
+
+  drawPaddles() {
+    this.setRectangle(-6.75, -0.75, 0.15, 1.5);
+    this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, 4);
+    this.setRectangle(6.60, -0.75, 0.15, 1.5);
+    this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, 4);
+  }
+
+  drawBall() {
+
+  }
+
+  setRectangle(x, y, width, height) {
+    const positionBuffer = this.gl.createBuffer();
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, positionBuffer);
+    
+    var x1 = x;
+    var x2 = x + width;
+    var y1 = y;
+    var y2 = y + height;
+
+    const positions = [
+      x1,  y2,
+      x2,  y2,
+      x1, y1,
+      x2, y1,
+    ];
+
+    this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(positions), this.gl.STATIC_DRAW);
+    var numComponents = 2;          // 2 components per iteration
+    var type = this.gl.FLOAT;   // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0;        // start at the beginning of the buffer
+    this.gl.vertexAttribPointer(
+      this.programInfo.attribLocations.vertexPosition,
+      numComponents,
+      type,
+      normalize,
+      stride,
+      offset);
+    this.gl.enableVertexAttribArray(this.programInfo.attribLocations.vertexPosition);
+  }
+
 }
